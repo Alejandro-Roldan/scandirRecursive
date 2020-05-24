6cb6550ce7deba51d9c5ae7d45aab5867ecb014d
Alejandro Roldán <alej.roldan.trabajos@gmail.com>
2020-05-25 00:47:49 +0200

    Add files via upload

--- benchmarkdemo.py
+++ benchmarkdemo.py
@@ -1,64 +1,49 @@
 #!/usr/bin/env python3
 
+'''
+    Compare the scandir_recursive function against the listdir for depth 0 and
+    os.walk for different depths and measure the execution times.
+'''
+
 from scandirrecursive import scandir_recursive
 import os
 import time
 
 
 def walker(path, levels=1):
-    ''' Small recursive limited os.walk implementation '''
-    files=[]
-    directories=[]
+    tree =[]
 
     for dirpath, dirnames, filenames in os.walk(path):
         if dirpath[len(path):].count(os.sep) >= levels:
             del dirnames[:]
         for name in filenames:
-            files.append(os.path.join(dirpath, name))
+            tree.append(os.path.join(dirpath, name))
         for name in dirnames:
-            directories.append(os.path.join(dirpath, name))
+            tree.append(os.path.join(dirpath, name))
 
-    files.sort(key=str.lower)
-    directories.sort(key=str.lower)
-
-    return files, directories
+    return tree
 
 def lister(path):
-    '''
-        listdir that outputs in the same way as my scandir_recursive
-        (a directories list and a files list with absolute paths)
-    '''
-    files=[]
-    directories=[]
+    tree = [item for item in os.listdir(path)]
 
-    for item in os.listdir(path):
-        if os.path.isdir(os.path.join(path, item)): directories.append(item)
-        else: files.append(item)
-
-    files.sort(key=str.lower)
-    directories.sort(key=str.lower)
-
-    return files, directories
+    return tree
 
 
 ###########################################################################
 ###########################################################################
-'''
-    Compare the scandir_recursive function against the listdir for depth 0 and
-    os.walk for different depths and time the execution times.
-'''
-path = '/'
+
+path = '/proc'
 
 ###########################################################################
 # DEPTH 0
 start = time.time()
-dirs, files = scandir_recursive(path, depth=0)
+tree = scandir_recursive(path, depth=0)
 end = time.time() - start
 print('scandir_recursive (depth=0):')
 print('\ttime: {:.3}s'.format(end))
 
 start = time.time()
-dirs, files = lister(path)
+tree = lister(path)
 end = time.time() - start
 print('listdir (not recursive func):')
 print('\ttime: {:.3}s'.format(end))
@@ -66,13 +51,13 @@ print('\ttime: {:.3}s'.format(end))
 print('-'*50)##############################################################
 # DEPTH 6
 start = time.time()
-dirs, files = scandir_recursive(path, depth=6)
+tree = scandir_recursive(path, depth=6)
 end = time.time() - start
 print('scandir_recursive (depth=6):')
 print('\ttime: {:.3}s'.format(end))
 
 start = time.time()
-dirs, files = walker(path, levels=6)
+tree = walker(path, levels=6)
 end = time.time() - start
 print('walk_limited (depth=6):')
 print('\ttime: {:.3}s'.format(end))
@@ -80,13 +65,13 @@ print('\ttime: {:.3}s'.format(end))
 print('-'*50)##############################################################
 # MAS DEPTH
 start = time.time()
-dirs, files = scandir_recursive(path, depth=-1)
+tree = scandir_recursive(path, depth=-1)
 end = time.time() - start
 print('scandir_recursive (max depth):')
 print('\ttime: {:.3}s'.format(end))
 
 start = time.time()
-dirs, files = walker(path, levels=999999999999)
+tree = walker(path, levels=999999999999)
 end = time.time() - start
 print('walk (max depth):')
 print('\ttime: {:.3}s'.format(end))
--- scandirrecursive.py
+++ scandirrecursive.py
@@ -1,89 +1,119 @@
 #!/usr/bin/env python3
 
 import os
+import re
 
-def scandir_recursive(path, depth=1, rtrn_names=False):
+def scandir_recursive_sorted(path, mask=re.compile(''), ext_tuple=[], folders=True,
+                            files=True, hidden=False, min_len=0,
+                            max_len=9999, depth=0, files_before_dirs=False):
+    ''' Create a scandir_recursive tree and sort it '''
+    tree = scandir_recursive(path, mask, ext_tuple, folders, files, hidden,
+                                min_len, max_len, depth)
+    tree = tree_sort(tree, depth, files_before_dirs)
+
+    return tree
+
+def scandir_recursive(path, mask=re.compile(''), ext_tuple=[], folders=True, files=True,
+                        hidden=False, min_len=0, max_len=9999, depth=0):
     '''
-        A scandir implementation that allows recursiveness by level.
+        A scandir implementation that allows recursiveness by level and returns
+        a list of os.DirEntry objects.
         Depth starts at the maximum value and goes down by one in each function
         call until it reaches 0 where it doesn't call the function anymore.
-        Call diagram for an example depth 1:
+
+        Returns a list of os.DirEntry objects.
+
+        Call diagram for an example depth=1:
         ***
-            Original call depth 1
+            Original call depth=1
                 finds a file -> add to list
                 finds folder -> add to list
                 depth > 0
-                    Call function depth 0
+                    Call function depth=depth-1=0
                         finds a file -> add to list
                         finds folder -> add to list
                         depth not > 0
-                            Doenst call function
-                    add the lists from last call to this calls lists
-                Return lists
+                            Doesn't call function
+                        Return list
+                    Add the lists from last call to this call list
+                Return list
         ***
 
-        If the depth is -1 does maximum recursiveness.
-
+        If the depth is -1 execute maximum recursiveness.
     '''
-    def _scandir_recursive(path, depth=1):
-        # Initialize empty lists
-        files=[]
-        directories=[]
+    tree = []
 
-        # loop through scandir
-        for entry in os.scandir(path):
-            if entry.is_file() or entry.is_symlink():
-                files.append(entry.path)
-            else:
-                directories.append(entry.path)
-                # For directories after adding them try to call the function
-                # again on the directory
-                try:
-                    # If depth is already 0 continue with the next step of
-                    # the loop
-                    if depth == 0:
-                        continue
-                    # If the depth is larger than 0 call the function again
-                    # with depth-1. Thatll produce that when it finds another
-                    # directory inside it will call the function with
-                    # (depth-1)-1. Itll do that until there are no more folders
-                    # in which case it will go back up to where it left of
-                    # and repeat
-                    elif depth > 0:
-                        files1, directories1 = _scandir_recursive(entry.path, depth-1)
-                    # And if depth is -1 call the function again with depth -1.
-                    # This will cause to call the function in every possible
-                    # folder
-                    elif depth == -1:
-                        files1, directories1 = _scandir_recursive(entry.path, -1)
+    # loop through scandir
+    for entry in os.scandir(path):
+        # BIIIG filter logic check to add entries
+        if (mask.match(entry.name) and
+            (not ext_tuple or entry.name.endswith(ext_tuple)) and
+            ((folders and entry.is_dir(follow_symlinks=False)) or
+            (files and entry.is_file())) and
+            (hidden or not entry.name.startswith('.')) and
+            (len(entry.name) > min_len and len(entry.name) < max_len)):
 
-                    # Now add the files and directories from that subfolder to
-                    # the list in this level
-                    files = files + files1
-                    directories = directories + directories1
+            tree.append(entry)
 
-                # Unless it catches any of this errors
-                except (FileNotFoundError, NotADirectoryError, PermissionError):
-                    pass
+        # When entry.is_dir
+        if (entry.is_dir(follow_symlinks=False) and (hidden or not entry.name.startswith('.'))):
+            # Try calling the function again inside the directory
+            try:
+                # If depth is already 0 skip and continue with the next step
+                # of the loop
+                if depth == 0: continue
+                # If the depth is larger than 0 call the function again
+                # with depth-1. That'll produce that when it finds another
+                # directory inside it it will call the function with
+                # (depth-1)-1. It'll do that until there are no more folders
+                # in which case it will go back up to where it left off
+                # and repeat
+                elif depth > 0: next_depth = depth - 1
+                # And if depth is -1 call the function again with depth=-1.
+                # This will cause to call the function in every possible
+                # folder
+                elif depth == -1: next_depth = -1
 
-        return files, directories
+                tree1 = scandir_recursive(entry.path, mask=mask,
+                                            ext_tuple=ext_tuple,
+                                            folders=folders, files=files,
+                                            hidden=hidden, min_len=min_len,
+                                            max_len=max_len, depth=next_depth)
+                tree = tree + tree1
 
-    directories, files = _scandir_recursive(path, depth)
+            # Unless it catches any of this errors
+            except (FileNotFoundError, NotADirectoryError, PermissionError):
+                pass
 
-    # Once the whole lists have been created, sort them
-    files.sort(key=str.lower)
-    directories.sort(key=str.lower)
+    return tree
 
-    # Transform them to basenames
-    if rtrn_names:
-        files = _split(files)
-        directories = _split(directories)
+def tree_sort(tree, depth=-1, files_before_dirs=False):
+    ''' Sort the tree list '''
+    # When depth wasn't 0 sort alphabetically and case-insensitively
+    # the absolute paths, this will produce having a folder followed by
+    # its contents
+    if depth != 0: tree.sort(key=lambda entry: entry.path.lower())
+    # When the depth was 0 separate the tree into files and directories, sort
+    # each of them alphabetically case-insensitive, and then join them
+    # together again
+    else:
+        files = []
+        directories = []
+        # Separate
+        for entry in tree:
+            if entry.is_file(): files.append(entry)
+            elif entry.is_dir(follow_symlinks=False): directories.append(entry)
+            # Sort
+            files.sort(key=lambda item: item.name.lower())
+            directories.sort(key=lambda item: item.name.lower())
 
-    return files, directories
+        # Join again files before dirs or viceversa
+        if files_before_dirs:
+            tree = files + directories
+        else:
+            tree = directories + files
 
-def _split(list_):
-    ''' Takes a list of absolute paths and returns the basename '''
-    return [os.path.split(item)[1] for item in list_]
+    return tree
 
 ###############################################################################
 '''
@@ -99,35 +129,64 @@ def main():
     parser = argparse.ArgumentParser()
     # Define the arguments
     # Obligatory argument, path
-    parser.add_argument("path", help="path to scan",
+    parser.add_argument('path', help='Path to scan',
                         type=str)
     # Optional argument, depth, defaults to 1 if not passed
-    parser.add_argument("-d", "--depth", help="levels of recursiveness",
+    parser.add_argument('-d', '--depth', help='Levels of recursiveness',
                         type=int, default=1)
-    # Switch to return names instead of absolute paths
-    parser.add_argument("-p", "--returnnames", help="output basenames",
-                    action="store_true")
+    # Check the filename against a regular expression
+    parser.add_argument('--mask', help='Filters the filenames with regular expressions',
+                        type=str, default='')
+    # Return the files that have any of the extensions from the list you provide
+    parser.add_argument('--exts', help='Filter by extension. Takes a list of extensions you want',
+                        type=list)
+    # Create a mutually exclusive group
+    group = parser.add_mutually_exclusive_group()
+    # Show folders, exclusive of showing files
+    group.add_argument('--folders', help="Don't show folders. Only usable without the --files switch", action='store_false')
+    # Show files, exclusive of showing folders
+    group.add_argument('--files', help="Don't show files. Only usable without the --folders switch", action='store_false')
+    # Default to false and then interchange it when passing the value to the
+    # function because it makes sense to show files when activating the
+    # --files switch and viceversa, instead of only showing something when
+    # activating the switch or turning them off when activating it
+    # Show hidden directories and files
+    parser.add_argument('--hidden', help='Show and access hidden files and folders', action='store_true')
+    # Minimum filename length
+    parser.add_argument('--min_len', help='Minimum filename length', type=int, default=0)
+    # Maximum filename lenth
+    parser.add_argument('--max_len', help='Maximum filename length', type=int, default=9999)
+    # Show files before directories, only when depth=0 
+    parser.add_argument('--files_before_dirs', help='Only avaible for depth=0. If active will show files before directories', action='store_true')
     # Switch to only output execution timing
-    parser.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument('-q', '--quiet', action='store_true')
+
     args = parser.parse_args()
 
+    # Compile the regular expressions mask
+    mask = re.compile(args.mask)
+
+    # Transform the list of extensions into a tuple and add a . before the
+    # extension to make sure that its an extension and not just that the
+    # filename ends in that
+    if args.exts: ext_tuple = tuple(['.' + i for i in args.exts])
+    else: ext_tuple = tuple()
 
     start = time.time()
-    files, directories= scandir_recursive(
-                                            args.path,
-                                            depth=args.depth,
-                                            rtrn_names=args.returnnames
-                                            )
+    tree = scandir_recursive_sorted(path=args.path, mask=mask,
+                                    ext_tuple=ext_tuple, folders=args.files,
+                                    files=args.folders, hidden=args.hidden,
+                                    min_len=args.min_len, max_len=args.max_len,
+                                    depth=args.depth,
+                                    files_before_dirs=args.files_before_dirs)
     end = time.time() - start
 
-    # Join file and dirs into one list and sort it
-    files_and_dirs = sorted(directories + files, key=str.lower)
     if not args.quiet:
         # Output the list
-        for item in files_and_dirs:
+        for entry in tree:
             try:
-                print(item)
-            # Skips files with non-UTF-8 names
+                print(entry.path)
+            # Skips files that the name can't be shown in the terminal
             except UnicodeEncodeError:
                 pass
         print()
